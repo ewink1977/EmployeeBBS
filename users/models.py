47df37e517f8621247b0df1dde653d48ab2a3d9b
Erin Winking <ewink@yahoo.com>
2021-02-17 15:27:21 -0800

    Issue #7 resolved by creating a custom template filter. Other minor template changes.

--- users/models.py
+++ users/models.py
@@ -38,3 +38,5 @@ class UserTimeManagement(models.Model):
     note = models.CharField(max_length = 255, blank = True, null = True)
     created_at = models.DateTimeField(auto_now_add = True)
     updated_at = models.DateTimeField(auto_now = True)
+
+    
---
+++ users/templatetags/durations.py
@@ -0,0 +1,10 @@
+from django import template
+register = template.Library()
+
+@register.filter
+def duration(td):
+    total_seconds = int(td.total_seconds())
+    hours = total_seconds // 3600
+    minutes = (total_seconds % 3600) // 60
+
+    return '{}:{}'.format(hours, minutes)
